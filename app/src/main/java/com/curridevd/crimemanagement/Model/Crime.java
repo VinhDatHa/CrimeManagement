33ad457987eba1d78cdd2adade4344156320bbc1
CurriHa <vinhdatmistake@gmail.com>
2020-12-18 16:05:03 +0700

    Modify UI of fragment

--- app/src/main/java/com/curridevd/crimemanagement/Model/Crime.java
+++ app/src/main/java/com/curridevd/crimemanagement/Model/Crime.java
@@ -1,18 +1,39 @@
 package com.curridevd.crimemanagement.Model;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	private UUID mID;
 	private String mTitle;
+	private Date mDate;
+	private boolean isSolved;
 
-	public Crime( String mTitle) {
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date mDate) {
+		this.mDate = mDate;
+	}
+
+	public boolean isSolved() {
+		return isSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		isSolved = solved;
+	}
+
+	public Crime(String mTitle) {
 		this.mID = UUID.randomUUID();
 		this.mTitle = mTitle;
 	}
 
 	public Crime (){
 		this.mID = UUID.randomUUID();
+		this.mDate = new Date();
 	}
 
 	public UUID getID() {
